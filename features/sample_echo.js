330537156070b55d33c92810eb84d24392c9c70d
dancreppel <creppel@usc.edu>
2020-10-21 18:27:20 -0700

    Disable sample features
    Co-authored-by: alexbpbdroid <alexbpbdroid@gmail.com>

--- features/sample_echo.js
+++ features/sample_echo.js
@@ -5,17 +5,17 @@
 
 module.exports = function(controller) {
 
-    controller.hears('sample','message,direct_message', async(bot, message) => {
-        await bot.reply(message, 'I heard a sample message.');
-    });
+    // controller.hears('sample','message,direct_message', async(bot, message) => {
+    //     await bot.reply(message, 'I heard a sample message.');
+    // });
 
-    controller.hears("test", 'message', async(bot, message) => {
-      await bot.beginDialog('typing');
-      await bot.reply(message, 'Testing 123');
-    });
+    // controller.hears("test", 'message', async(bot, message) => {
+    //   await bot.beginDialog('typing');
+    //   await bot.reply(message, 'Testing 123');
+    // });
 
-    controller.on('message,direct_message', async(bot, message) => {
-        await bot.reply(message, `Echo: ${ message.text }`);
-    });
+    // controller.on('message,direct_message', async(bot, message) => {
+    //     await bot.reply(message, `Echo: ${ message.text }`);
+    // });
     
 }
