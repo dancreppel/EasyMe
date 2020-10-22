8b4b1282728f530875d54032796b56662d2d403e
dancreppel <creppel@usc.edu>
2020-10-21 18:24:49 -0700

    Create dialog for social links
    Co-authored-by: alexbpbdroid <alexbpbdroid@gmail.com>

---
+++ features/info_skills.js
@@ -0,0 +1,30 @@
+const { BotkitConversation } = require("botkit");
+const { skills } = require("../resume.json");
+
+module.exports = (controller) => {
+  const CONVO_ID = "skills";
+  let convo = new BotkitConversation(CONVO_ID, controller);
+
+  let quick_replies = [];
+  skills.forEach((el) => {
+    let option = {
+      title: el.name,
+      payload: el.name,
+    };
+
+    quick_replies.push(option);
+
+    controller.hears(el.name, "message", async (bot, message) => {
+      await bot.beginDialog("typing");
+      await bot.beginDialog("followUpSkills");
+    });
+  });
+
+  let options = {
+    text: "Here is the list of languages and frameworks I am proficient with?",
+    quick_replies,
+  };
+
+  convo.addMessage(options);
+  controller.addDialog(convo);
+};
