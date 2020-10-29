a925fb80d29bfdbdeea76875e74e86113157c73e
dancreppel <creppel@usc.edu>
2020-10-28 18:58:16 -0700

    Add logic to handle building branches for arrays

--- helpers/build_branches.js
+++ helpers/build_branches.js
@@ -4,11 +4,61 @@ const { BotkitConversation } = require('botkit');
 
 // note: this function will be called within a feature, so controller will be provided
 module.exports = function buildBranches (node, controller) {
-  if (typeof node.value === 'object') {
+  if (Array.isArray(node.value)) {
+    let convoId = node.name;
+    let convo = new BotkitConversation(convoId, controller);
     const quickReplies = [];
-    // Build a conversation
-    let CONVO_ID = node.name;
-    let convo = new BotkitConversation(CONVO_ID, controller);
+
+    node.value.forEach((obj, nodeIdx) => {
+      let objKeys = Object.keys(obj);
+      // the first key is the identifier 
+      let identifier = objKeys[0];
+      let quickReply = {
+        title: obj[identifier],
+        payload: obj[identifier]
+      }
+
+      quickReplies.push(quickReply);
+
+      // create a psuedo node in which the identifier is the name of the node
+      // and the rest of the keys are the values associated with said node
+
+      // prevent changes to original object
+      Object.freeze(obj);
+      //create a copy of the obj
+      let value = Object.assign({}, obj);
+      // delete the identifier which is the first field
+      delete value[identifier];
+
+      let child = new Node(node, obj[identifier], value);
+      buildBranches(child, controller);
+    });
+    
+    let options = {
+      text: `What would you like to know about ${node.name}?`,
+      quick_replies: quickReplies
+    }
+
+    convo.addMessage(options);
+    controller.addDialog(convo);
+
+    // Add listener for convo
+    controller.hears(node.name, 'message', async (bot, message) => {
+      await bot.beginDialog('typing');
+      await bot.beginDialog(node.name);
+    });
+
+
+    // Create follow up dialog
+    followUp(node, controller);
+
+    // no need to return anything because parent doesn't need to know about
+    // child's quick replies
+    return null;
+  } else if (typeof node.value === 'object') { 
+    const quickReplies = [];
+    const convoId = node.name;
+    const convo = new BotkitConversation(convoId, controller);
 
     for (const key in node.value) {
       let child = new Node(node, key, node.value[key])
@@ -34,7 +84,7 @@ module.exports = function buildBranches (node, controller) {
     }
 
     let options = {
-      text: "What would you like to know?",
+      text: `What would you like to know about ${node.name}?`,
       quick_replies: quickReplies
     }
 
@@ -57,11 +107,6 @@ module.exports = function buildBranches (node, controller) {
     // BASE CASE
     // In this case the node's value is a string, which is the value of the key
     // from it's parent's node
-    if (node.name === 'phone') {
-      console.log(node.name);
-      console.log(typeof node.value);
-      console.log(node.value);
-    }
 
     if (node.value === "") {
       return `error: in the resume json file, value must not be empty for the 
